aa3c930e26d77cf1950f120ad7922ba0aa40826e
NahueB <80709577+NahueB@users.noreply.github.com>
2022-09-17 15:20:21 -0300

    Add files via upload

---
+++ src/Parte1.java
@@ -0,0 +1,25 @@
+public class Parte1
+{
+    public static void main(String[] args)
+    {
+        int suma=suma(1,2,3);
+        System.out.println("La suma es:"+suma);
+
+        Coche micoche=new Coche();
+        micoche.AnadirPuerta();
+        System.out.println("El numero de puertas es: " + micoche.puerta);
+    }
+    public static int suma(int a, int b,int c)
+    {
+        return a+b+c;
+    }
+}
+class Coche
+{
+    public int puerta = 4;
+
+    public void AnadirPuerta()
+    {
+        this.puerta++;
+    }
+}
